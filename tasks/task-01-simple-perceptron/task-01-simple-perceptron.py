9a03c899164967aefcce98f590f30f8839bc9555
Fabiano <radbios@gmail.com>
2025-07-31 19:21:57 -0300

    feat: change ephocs

--- tasks/task-01-simple-perceptron/task-01-simple-perceptron.py
+++ tasks/task-01-simple-perceptron/task-01-simple-perceptron.py
@@ -110,7 +110,7 @@ def main():
     X, y = generate_data(39)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = Perceptron(epochs=10000)
+    model = Perceptron(epochs=100)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     accuracy = np.mean(predictions == y_test)
